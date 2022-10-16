70bfdd0e6f6d9a70d923f6ea4765f65abe81ad57
Piotr Pęgiel <p.a.pegiel@gmail.com>
2022-10-16 20:11:24 +0200

    before class 3

--- 02-TypesAndVariables/17.py
+++ 02-TypesAndVariables/17.py
@@ -5,4 +5,4 @@ import math
 wzrost = int(input('Podaj swój wzrost w cm: '))
 cale, stopy = math.modf(wzrost/30.48)
 cale=round(cale, 2)*12
-print(f'Mój wzrost to {wzrost}cm. Dla tych gorszych to {int(stopy)} stóp i {cale} cali')
+print(f'Mój wzrost to {wzrost}cm. Dla tych gorszych to {int(stopy)} stóp i {round(cale,2)} cali')
